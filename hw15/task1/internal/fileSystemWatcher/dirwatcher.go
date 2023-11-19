1ba69b893f4e608379991bce07141d50382c6a57
unknown <dzhurynskyi.k@brights.cloud>
2023-11-19 20:22:05 +0200

    hw16: gorm

--- hw15/task1/internal/fileSystemWatcher/dirwatcher.go
+++ hw15/task1/internal/fileSystemWatcher/dirwatcher.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	watcherstate "qeueu/task1/internal/watcherState"
 	"qeueu/task1/pkg/pubsub"
 )
 
@@ -37,8 +38,9 @@ func (f *FileWatcher) Watch() {
 func NewDirWatcher(path string) *DirWatcher {
 	d := new(DirWatcher)
 	broker := pubsub.NewBroker()
-
-	d.walkThroughDirs(path, broker)
+	state := watcherstate.NewGormState()
+	state.InitDatabase(path)
+	d.walkThroughDirs(path, broker, state)
 
 	for i := range d.files {
 		d.files[i].Watch()
@@ -53,7 +55,7 @@ func NewDirWatcher(path string) *DirWatcher {
 	}
 }
 
-func (d *DirWatcher) walkThroughDirs(path string, broker *pubsub.Broker) {
+func (d *DirWatcher) walkThroughDirs(path string, broker *pubsub.Broker, state *watcherstate.GORMState) {
 	var files []FileWatcher
 	entries, err := os.ReadDir(path)
 	if err != nil {
@@ -61,18 +63,29 @@ func (d *DirWatcher) walkThroughDirs(path string, broker *pubsub.Broker) {
 	}
 
 	for _, e := range entries {
-		if e.Type().IsDir() {
-			d.walkThroughDirs(path+"/"+e.Name(), broker)
-		}
 		if e.Type().IsRegular() {
 			fileName := path + "/" + e.Name()
-			fmt.Println(fileName, " is ADDED to tracking")
+			log.Println(fileName, "is ADDED to tracking")
 			stat, _ := os.Stat(fileName)
 			files = append(files, FileWatcher{
 				fileName: fileName,
 				lmt:      stat.ModTime().Nanosecond(),
 				producer: pubsub.Producer{Broker: broker},
 			})
+			currentDir := state.FindDir(path)
+			state.SaveFile(watcherstate.File{
+				FileName: e.Name(),
+				Path:     fileName,
+				DirID:    currentDir.ID,
+				LMT:      stat.ModTime(),
+			})
+		}
+		if e.Type().IsDir() {
+			state.SaveDir(watcherstate.Dir{
+				WatcherID: state.Watcher.ID,
+				Path:      path + "/" + e.Name(),
+			})
+			d.walkThroughDirs(path+"/"+e.Name(), broker, state)
 		}
 	}
 	d.files = append(d.files, files...)
---
+++ hw15/task1/internal/watcherState/init.go
@@ -0,0 +1,67 @@
+// Додати роботу з базою даних до домашньої роботи із домашнього завдання вебінару 15:
+// • створити базу і щонайменше 3 таблиці;
+// • у коді виконаного завдання підʼєднатися до бази;
+// • кодом додавати записи до таблиць і вибирати їх.
+
+package watcherstate
+
+import (
+	"log"
+	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Dir struct {
+	gorm.Model
+	WatcherID uint
+	Path      string
+	Files     []File `gorm:"foreignKey:DirID"`
+}
+
+type File struct {
+	gorm.Model
+	FileName string
+	Path     string
+	Dir      Dir
+	DirID    uint
+	LMT      time.Time
+}
+
+type Watcher struct {
+	gorm.Model
+	Dirs   []Dir  `gorm:"foreignKey:WatcherID"`
+	Logger Logger `gorm:"foreignKey:WatcherID"`
+}
+
+type Logger struct {
+	gorm.Model
+	WatcherID uint
+}
+
+type GORMState struct {
+	db      *gorm.DB
+	RootDir *Dir
+	Watcher *Watcher
+}
+
+func NewGormState() *GORMState {
+	db, err := gorm.Open(sqlite.Open("watcher.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&Watcher{}, &Logger{}, &Dir{}, &File{})
+
+	return &GORMState{
+		db: db,
+	}
+}
+func (g *GORMState) InitDatabase(rootPath string) {
+	g.Watcher = g.SaveWatcher(Watcher{})
+	g.RootDir = g.SaveDir(Dir{
+		WatcherID: g.Watcher.ID,
+		Path:      rootPath,
+	})
+}
